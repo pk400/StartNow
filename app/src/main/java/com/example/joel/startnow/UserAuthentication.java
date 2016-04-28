e4ec9e3ca86cab6bca364a97863933e88b44545f
pk400 <joelaro@outlook.com>
2016-04-28 18:16:13 -0400

    Started user authentication

---
+++ app/src/main/java/com/example/joel/startnow/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com.example.joel.startnow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Joel on 4/28/2016.
+ */
+public class UserAuthentication extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_authentication);
+    }
+}
