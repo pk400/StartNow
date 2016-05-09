436ecbd0199376e6310e6f516acb5a47b53ce959
Magic17 <this_is_magic@hotmail.com>
2016-05-09 18:57:31 -0400

    added public profile activity

---
+++ app/src/main/java/com/example/joel/startnow/PubProfile.java
@@ -0,0 +1,16 @@
+package com.example.joel.startnow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Magic on 5/9/2016.
+ */
+public class PubProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pub_profile);
+    }
+}
